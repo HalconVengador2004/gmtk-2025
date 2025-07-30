b648d1cf0502f4dffc631bdc2bfcaaf7d3c38b10
Ivan Nieto <54584215+HalconVengador2004@users.noreply.github.com>
2025-07-30 21:38:33 +0200

    Added a clickable component (#1)
    * Added a clickable component
    
    * github actions
    
    * fix github actions
    
    * Deleted workflows

---
+++ gmtk-2025/scenes/components/clickable_component.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var click_area
+
+func _ready():
+	click_area = get_parent() as Area2D
+	assert(click_area, "Please assign a click area (Area2D) as a parent of this node")
+	click_area.input_event.connect(_on_Area2D_input_event)
+
+func _on_Area2D_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("Sprite clicked!")
+			SignalBus.emit_signal("entity_clicked")
