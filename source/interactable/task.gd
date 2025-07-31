e140903b4a5a59c3151b2119f44e9a7c9c641c82
HalconVengador2004 <i.nieto@kevull.com>
2025-07-31 13:39:26 +0200

    Added navigation

--- source/interactable/task.gd
+++ source/interactable/task.gd
@@ -2,9 +2,16 @@ class_name Task
 
 var resource: TaskResource
 var progress: float = 0.0
+var is_assigned: bool = false
 
 func _init(task_resource: TaskResource):
 	self.resource = task_resource
 
 func is_complete() -> bool:
 	return progress >= resource.time_to_finish
+
+func is_available() -> bool:
+	return not is_complete() and not is_assigned
+	
+func set_is_assigned(assigned):
+	is_assigned = assigned
--- source/scenes/game/worker_manager.gd
+++ source/scenes/game/worker_manager.gd
@@ -1,6 +1,27 @@
 extends Node2D
+class_name WorkerManager
 
-var workers
+var selected_worker
 
 func _ready():
-	var workers = get_tree().get_nodes_in_group("worker")
+	SignalBus.connect("worker_clicked", select_worker)
+	SignalBus.connect("task_clicked", move_worker_to_task)
+
+func select_worker(clicked_worker):
+	selected_worker = clicked_worker
+
+func move_worker_to_task(task_instance):
+	if not selected_worker:
+		return
+		
+	var previous_task = selected_worker.get_assigned_task()
+	if previous_task:
+		previous_task.task_data.set_is_assigned(false)
+		
+	if not task_instance.task_data.is_available():
+		print("task is not available")
+		return
+		
+	selected_worker.set_navigation_destination(task_instance.global_position)
+	task_instance.task_data.set_is_assigned(true)
+	selected_worker.set_assigned_task(task_instance)
