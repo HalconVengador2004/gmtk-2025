10db7bbcbd72c869f9b50229991a2147cf8f8cd2
HalconVengador2004 <i.nieto@kevull.com>
2025-08-01 16:08:34 +0200

    Implemented basic difficulty system that increases frequency

--- source/scripts/task_manager.gd
+++ source/scripts/task_manager.gd
@@ -1,14 +1,17 @@
 extends Node2D
 class_name TaskManager
 
-@export var break_interval: float = 6.0
+@export var initial_break_interval: float = 10.0
+@export var interval_decay_factor: float = 0.99
 var tasks: Array[Node] = []
 var timer: Timer
+var current_break_interval: float
 
 func _ready():
 	tasks = get_tree().get_nodes_in_group("task")
 	for task in tasks:
 		task.connect("break_random_task", _break_random_task)
+	current_break_interval = initial_break_interval
 	start_default_breaking_timer()
 
 func _break_random_task():
@@ -21,12 +24,23 @@ func _break_random_task():
 		pick.break_task()
 		print("breaking task")
 	else:
+		#TODO actually losing
 		print("YOU LOST")
 
-func start_default_breaking_timer() -> void:
+func start_default_breaking_timer():
 	if not timer:
 		timer = Timer.new()
 		add_child(timer)
-	timer.wait_time = break_interval
+	timer.wait_time = initial_break_interval
 	timer.start()
-	timer.timeout.connect(_break_random_task)
+	timer.timeout.connect(_on_timer_timeout)
+	
+func increase_difficulty():
+	current_break_interval *= interval_decay_factor
+	
+func _on_timer_timeout():
+	_break_random_task()
+	increase_difficulty()
+	# restart with new break interval
+	timer.wait_time = current_break_interval
+	timer.start()
