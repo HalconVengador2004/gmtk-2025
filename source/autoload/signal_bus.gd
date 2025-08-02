4a834fe789e6647dd8e781156065a6c0982d45c1
Ivan Nieto <54584215+HalconVengador2004@users.noreply.github.com>
2025-08-02 17:42:09 +0200

    Dev ivan (#16)
    * Fixed task progress bar not showing
    
    * Fixed task progress bar not showing
    
    * Added movement from workers to tasks

--- source/autoload/signal_bus.gd
+++ source/autoload/signal_bus.gd
@@ -2,7 +2,9 @@ extends Node
 
 signal worker_clicked(worker)
 signal task_clicked(task)
+signal task_clicked_instance(task_instance: TaskInstance)
 signal storage_clicked(storage)
 signal task_activated(task)
 signal clicked(entity)
 signal day_changed(new_day)
+signal task_instance_created(task_instance: TaskInstance)
