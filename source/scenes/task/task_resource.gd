05aee2e054da2181ffbc70c8930ccf2cc56e3d5b
mororo250 <joao_marcos_8@hotmail.com>
2025-08-03 07:04:58 -0300

    more fixes

--- source/scenes/task/task_resource.gd
+++ source/scenes/task/task_resource.gd
@@ -5,3 +5,4 @@ class_name TaskResource
 @export var required_item: ItemResource
 @export var time_to_finish: float = 5.0 # Time in seconds
 @export var deadline: float = 30.0 # Time in seconds
+@export var icon: Texture2D
