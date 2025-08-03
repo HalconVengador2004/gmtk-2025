3d4f8dcf9fb70a8b627d50e7c912c00a6f0ed497
HalconVengador2004 <i.nieto@kevull.com>
2025-08-03 17:19:35 +0200

    Fixed item size and added sprites

--- source/scenes/item/item.gd
+++ source/scenes/item/item.gd
@@ -13,6 +13,9 @@ func _ready():
 func init(new_resource: ItemResource):
 	resource = new_resource
 	sprite.texture = resource.texture
+	var small_size = Vector2(64, 64)
+	var texture_size = sprite.texture.get_size()
+	sprite.scale = small_size / texture_size
 	
 func clear():
 	resource = null
