815a9471dc440e34754db870f2d92d1b9091e789
mororo250 <joao_marcos_8@hotmail.com>
2025-08-03 06:48:44 -0300

    Refactor task system: add progress tracking, integrate icons, manage task deadlines and overdue states, optimize Clock and TimeManager classes, and update scene dependencies accordingly.

--- source/scenes/task/task.gd
+++ source/scenes/task/task.gd
@@ -1,31 +1,39 @@
 class_name Task
 
 var resource: TaskResource
-var progress: float = 0.0
-var is_assigned: bool = false
+var time_working: float = 0.0
+var is_assigned: bool   = false
 var start_time: float = 0.0
+var is_overdue_checked: bool = false
 
 func _init(task_resource: TaskResource):
 	self.resource = task_resource
 
+func reset():
+	time_working = 0.0
+	is_assigned = false
+	start_time = 0.0
+	is_overdue_checked = false
+
 func is_overdue() -> bool:
-	if is_assigned:
-		return Time.get_ticks_msec() - start_time > resource.deadline * 1000
+	if is_assigned and not is_overdue_checked:
+		var overdue = TimeManager.get_time() - start_time > resource.deadline
+		if overdue:
+			is_overdue_checked = true
+			SignalBus.task_is_overdue.emit(self)
+		return overdue
 	return false
 
 func is_complete() -> bool:
-	return progress >= resource.time_to_finish
-	
-func set_progress(prog):
-	progress = prog
+	return time_working >= resource.time_to_finish
 
-func get_progress():
-	return progress
+func get_time_working():
+	return time_working
 	
 func set_is_assigned(assigned):
 	is_assigned = assigned
 	if is_assigned:
-		start_time = Time.get_ticks_msec()
+		start_time = TimeManager.get_time()
 
 func get_is_assigned():
 	return is_assigned
