ea8c7ce3ba2f368e45e77fb66f677ef84d52f615
Kyle Lankton <kylelankton@gmail.com>
2025-08-03 06:40:22 -0500

    audio_manager changes

--- source/scenes/game/audio_manager.gd
+++ source/scenes/game/audio_manager.gd
@@ -2,26 +2,26 @@ extends Node
 
 @export var music_player: AudioStreamPlayer
 var intensity_state : String
-var sfx_library = {
-	"workerassign":preload("res://source/scenes/game/game.tscn::AudioStreamRandomizer_2jurd"),
-	"workercomplete":preload("res://source/scenes/game/game.tscn::AudioStreamRandomizer_b0not"),
-	"workerselect":preload("res://source/scenes/game/game.tscn::AudioStreamRandomizer_b7cst"),
-	"workerwork":preload("res://assets/audio/sfx/Worker/sfx_WorkerWork.ogg"),
-	"problemalert":preload("res://assets/audio/sfx/Problem/sfx_ProblemAlert.ogg"),
-	"problembroken":preload("res://source/scenes/game/game.tscn::AudioStreamRandomizer_vrq42"),
-	"problemstart":preload("res://source/scenes/game/game.tscn::AudioStreamRandomizer_ftnvo"),
-	"uiconfirm":preload("res://assets/audio/sfx/UI/sfx_UIConfirm.ogg"),
-	"uiselect":preload("res://source/scenes/game/game.tscn::AudioStreamRandomizer_j74dg"),
-	"uideselect":preload("res://source/scenes/game/game.tscn::AudioStreamRandomizer_e1c0h")
-	}
 
-
-func createVfx(name):
+func createVfx(sfx_library):
 	var new_sfx_player = $SfxPlayer.duplicate()
-	new_sfx_player.stream = sfx_library
+	new_sfx_player.stream = sfx_library[sfx_library]
 	add_child(new_sfx_player)
 	new_sfx_player.play()
 	new_sfx_player.finished.connect(_on_sfx_finished)
+
+var sfx_library = {
+	"workerassign":preload("res://assets/audio/sfx/Worker/sfx_WorkerAssign_2-001.ogg"),
+	"workercomplete":preload("res://assets/audio/sfx/Worker/sfx_WorkerComplete_1.ogg"),
+	"workerselect":preload("res://assets/audio/sfx/Worker/sfx_WorkerSelect_1.ogg"),
+	"workerwork":preload("res://assets/audio/sfx/Worker/sfx_WorkerWork.ogg"),
+	"problemalert":preload("res://assets/audio/sfx/Problem/sfx_ProblemAlert.ogg"),
+	"problembroken":preload("res://assets/audio/sfx/Problem/sfx_ProblemBroken_2.ogg"),
+	"problemstart":preload("res://assets/audio/sfx/Problem/sfx_ProblemStart_3-001.ogg"),
+	"uiconfirm":preload("res://assets/audio/sfx/UI/sfx_UIConfirm.ogg"),
+	"uiselect":preload("res://assets/audio/sfx/UI/sfx_UIselect_1.ogg"),
+	"uideselect":preload("res://assets/audio/sfx/UI/sfx_UIdeselect_1.ogg")
+	}
 
 
 func on_working_start():
@@ -39,17 +39,16 @@ func _on_task_completed():
 func _on_task_started():
 	createVfx("problemstart")
 
-
 @onready var music_library = $MusicPlayer
 
 func _ready():
 	$MusicPlayer.play()
-	SignalBus.eventName.Conetct(functionName)
-	
-	
-	
-	
+	SignalBus.eventName.Conetct(on_working_start())
+	SignalBus.eventName.Conetct(_on_interactable_clicked())
+	SignalBus.eventName.Conetct(select_worker())
+	SignalBus.eventName.Conetct(_on_task_completed())
+	SignalBus.eventName.Conetct(_on_task_started())
+
+
 func _on_sfx_finished(node_to_free):
 	node_to_free.queue_free()
-	
-	
