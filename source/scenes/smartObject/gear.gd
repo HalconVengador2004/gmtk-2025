6c95f05d2270ce9a1c582d338a1010dde118bb48
mororo250 <joao_marcos_8@hotmail.com>
2025-08-11 22:08:13 -0300

    Fix gear rotating direction

--- source/scenes/smartObject/gear.gd
+++ source/scenes/smartObject/gear.gd
@@ -3,25 +3,30 @@ class_name Gear
 
 @export var multi_sprite: HighlightableMultiSprite
 @export var task_resource_2: TaskResource
-@export var rotation_speed: float = 1.0
+
+var gears_sprites: Array[GearSprite]
 
 var is_spinning: bool = true
-var children: Array[Node]
 var task_1: Task
 var task_2: Task
 var is_task_1_active: bool
 
+
 func _ready():
 	super()
-	children = multi_sprite.get_children()
+	var nodes: Array[Node] = multi_sprite.get_children()
+	for node in nodes:
+		if node is GearSprite:
+			gears_sprites.append(node)
+	
 	task_1 = Task.new(task_resource)
 	task_2 = Task.new(task_resource_2)
 
 func _process(delta):
 	super(delta)
 	if is_spinning:
-		for child in children:
-			child.rotation += delta * rotation_speed
+		for gearSprite in gears_sprites:
+			gearSprite.rotation += delta * gearSprite.rotation_speed
 			
 func get_task():
 	return task_1 if is_task_1_active else task_2
